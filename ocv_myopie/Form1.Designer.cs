9fd9899d7df75552b1cd0ea230003c2099fe3f3c
nbourre <nbourre@hotmail.com>
2017-02-27 14:02:40 -0500

    VErsion fonctionnelle

--- ocv_myopie/Form1.Designer.cs
+++ ocv_myopie/Form1.Designer.cs
@@ -34,17 +34,24 @@
             this.spc = new gei1076_tools.SerialPortConfigurator();
             this.vsbDioptrie = new System.Windows.Forms.VScrollBar();
             this.label1 = new System.Windows.Forms.Label();
+            this.txtDioptrie = new System.Windows.Forms.TextBox();
+            this.tmrReception = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDistance = new System.Windows.Forms.Label();
+            this.tabMain = new System.Windows.Forms.TabControl();
+            this.tabView = new System.Windows.Forms.TabPage();
+            this.tabConfiguration = new System.Windows.Forms.TabPage();
             ((System.ComponentModel.ISupportInitialize)(this.pbCamera)).BeginInit();
+            this.tabMain.SuspendLayout();
+            this.tabView.SuspendLayout();
+            this.tabConfiguration.SuspendLayout();
             this.SuspendLayout();
             // 
             // pbCamera
             // 
-            this.pbCamera.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pbCamera.Location = new System.Drawing.Point(13, 112);
+            this.pbCamera.Location = new System.Drawing.Point(3, 6);
             this.pbCamera.Name = "pbCamera";
-            this.pbCamera.Size = new System.Drawing.Size(499, 296);
+            this.pbCamera.Size = new System.Drawing.Size(640, 480);
             this.pbCamera.TabIndex = 0;
             this.pbCamera.TabStop = false;
             // 
@@ -57,20 +64,19 @@
             // 
             this.spc.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.spc.Location = new System.Drawing.Point(13, 13);
+            this.spc.Location = new System.Drawing.Point(6, 6);
             this.spc.Name = "spc";
-            this.spc.Size = new System.Drawing.Size(499, 93);
+            this.spc.Size = new System.Drawing.Size(707, 93);
             this.spc.TabIndex = 1;
             // 
             // vsbDioptrie
             // 
             this.vsbDioptrie.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.vsbDioptrie.LargeChange = 100;
-            this.vsbDioptrie.Location = new System.Drawing.Point(541, 57);
+            this.vsbDioptrie.Location = new System.Drawing.Point(680, 58);
             this.vsbDioptrie.Maximum = 1000;
-            this.vsbDioptrie.Minimum = -1000;
             this.vsbDioptrie.Name = "vsbDioptrie";
-            this.vsbDioptrie.Size = new System.Drawing.Size(39, 202);
+            this.vsbDioptrie.Size = new System.Drawing.Size(26, 207);
             this.vsbDioptrie.SmallChange = 25;
             this.vsbDioptrie.TabIndex = 2;
             this.vsbDioptrie.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vsbDioptrie_Scroll);
@@ -79,7 +85,7 @@
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(538, 29);
+            this.label1.Location = new System.Drawing.Point(672, 6);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 3;
@@ -87,23 +93,95 @@
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // txtDioptrie
+            // 
+            this.txtDioptrie.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDioptrie.Location = new System.Drawing.Point(671, 25);
+            this.txtDioptrie.Name = "txtDioptrie";
+            this.txtDioptrie.Size = new System.Drawing.Size(44, 20);
+            this.txtDioptrie.TabIndex = 4;
+            this.txtDioptrie.Text = "0";
+            this.txtDioptrie.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tmrReception
+            // 
+            this.tmrReception.Interval = 10;
+            this.tmrReception.Tick += new System.EventHandler(this.tmrReception_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(651, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Distance :";
+            // 
+            // lblDistance
+            // 
+            this.lblDistance.AutoSize = true;
+            this.lblDistance.Location = new System.Drawing.Point(680, 329);
+            this.lblDistance.Name = "lblDistance";
+            this.lblDistance.Size = new System.Drawing.Size(30, 13);
+            this.lblDistance.TabIndex = 5;
+            this.lblDistance.Text = "0 cm";
+            this.lblDistance.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // tabMain
+            // 
+            this.tabMain.Controls.Add(this.tabView);
+            this.tabMain.Controls.Add(this.tabConfiguration);
+            this.tabMain.Location = new System.Drawing.Point(12, 12);
+            this.tabMain.Name = "tabMain";
+            this.tabMain.SelectedIndex = 0;
+            this.tabMain.Size = new System.Drawing.Size(729, 527);
+            this.tabMain.TabIndex = 6;
+            // 
+            // tabView
+            // 
+            this.tabView.Controls.Add(this.pbCamera);
+            this.tabView.Controls.Add(this.lblDistance);
+            this.tabView.Controls.Add(this.label1);
+            this.tabView.Controls.Add(this.label2);
+            this.tabView.Controls.Add(this.vsbDioptrie);
+            this.tabView.Controls.Add(this.txtDioptrie);
+            this.tabView.Location = new System.Drawing.Point(4, 22);
+            this.tabView.Name = "tabView";
+            this.tabView.Padding = new System.Windows.Forms.Padding(3);
+            this.tabView.Size = new System.Drawing.Size(721, 501);
+            this.tabView.TabIndex = 0;
+            this.tabView.Text = "Vision";
+            this.tabView.UseVisualStyleBackColor = true;
+            // 
+            // tabConfiguration
+            // 
+            this.tabConfiguration.Controls.Add(this.spc);
+            this.tabConfiguration.Location = new System.Drawing.Point(4, 22);
+            this.tabConfiguration.Name = "tabConfiguration";
+            this.tabConfiguration.Padding = new System.Windows.Forms.Padding(3);
+            this.tabConfiguration.Size = new System.Drawing.Size(721, 501);
+            this.tabConfiguration.TabIndex = 1;
+            this.tabConfiguration.Text = "Configuration";
+            this.tabConfiguration.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(599, 420);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.vsbDioptrie);
-            this.Controls.Add(this.spc);
-            this.Controls.Add(this.pbCamera);
-            this.MinimumSize = new System.Drawing.Size(509, 459);
+            this.ClientSize = new System.Drawing.Size(754, 541);
+            this.Controls.Add(this.tabMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimumSize = new System.Drawing.Size(770, 580);
             this.Name = "Form1";
             this.Text = "Simulateur de myopie";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             ((System.ComponentModel.ISupportInitialize)(this.pbCamera)).EndInit();
+            this.tabMain.ResumeLayout(false);
+            this.tabView.ResumeLayout(false);
+            this.tabView.PerformLayout();
+            this.tabConfiguration.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -114,6 +192,13 @@
         private gei1076_tools.SerialPortConfigurator spc;
         private System.Windows.Forms.VScrollBar vsbDioptrie;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDioptrie;
+        private System.Windows.Forms.Timer tmrReception;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDistance;
+        private System.Windows.Forms.TabControl tabMain;
+        private System.Windows.Forms.TabPage tabView;
+        private System.Windows.Forms.TabPage tabConfiguration;
     }
 }
 
--- ocv_myopie/Form1.cs
+++ ocv_myopie/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenCvSharp;
-
+using gei1076_tools;
 
 namespace ocv_myopie
 {
@@ -18,24 +18,65 @@ namespace ocv_myopie
         Mat frame = new Mat();
 
         OpenCvSharp.Size kSize;
+        double sigma = 0.0;
 
-        
-        
+        PortSerie ps;
+        byte octet;
+        const int tailleTrame = 2;
+        private byte[] trame = new byte[tailleTrame];
+        private byte[] tableau = new byte[tailleTrame];
+
+        int cm = 0;
+
         public Form1()
         {
             InitializeComponent();
+            Binding binding = new Binding("Text", vsbDioptrie, "Value");
+
+            binding.Format += bind_Format;
+            binding.Parse += bind_Parse;
+
+            txtDioptrie.DataBindings.Add(binding);
+
+            
+        }
+
+        private void bind_Parse(object sender, ConvertEventArgs e)
+        {
+
+        }
+
+        private void bind_Format(object sender, ConvertEventArgs e)
+        {
+            double raw = -(int)(e.Value) / 100.0;
+
+            sigma = dyoptrieToSigma(raw);
+
+            if (cm > 0)
+            {
+                double metre = cm / 100.0;
+
+            }
+
+            Console.WriteLine("sigma = " + sigma);
+ 
+
+            e.Value = raw.ToString("0.00");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            kSize.Height = 5;
-            kSize.Width = 5;
+            ps = spc.getPS();
+            tmrReception.Enabled = true;
 
+            kSize.Height = 1;
+            kSize.Width = 1;
+            
             cap.Read(frame);
 
             Width = frame.Width + 50;
             Height = frame.Height + 60;
-            
+
             pbCamera.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(frame);
 
             tmrFrame.Enabled = true;
@@ -60,14 +101,13 @@ namespace ocv_myopie
 
         private Mat process(Mat src)
         {
-            
+
             if (src == null) return null;
 
             Mat result = new Mat();
 
-            Cv2.GaussianBlur(src, result, kSize, 1);
+            Cv2.GaussianBlur(src, result, kSize, sigma);
 
-            
             return result;
         }
 
@@ -79,6 +119,59 @@ namespace ocv_myopie
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        double dyoptrieToSigma(double dyoptrie)
+        {
+            // Source : http://esatjournals.net/ijret/2014v03/i17/IJRET20140317008.pdf
+            // Myopie : y = -6.921x + 0.0642
+            // Hypermétrie : y = 7.1401x + 0.657
+
+            // Source2 : view-source:http://www.billauer.co.il/simulator.html
+            if (dyoptrie == 0) return 0;
+
+            double resultat = 0.0;
+
+            if (dyoptrie < 0)
+            {
+                resultat = -6.921 * dyoptrie + 0.0642 - 1;
+            } else
+            {
+                resultat = 7.1401 * dyoptrie + 0.657 - 1;
+            }
+
+            kSize.Width = kSize.Height = (int)(resultat) % 2 == 0 ? (int)resultat + 1 : (int)resultat;
+
+            return resultat;
+
+        }
+
+        private void tmrReception_Tick(object sender, EventArgs e)
+        {
+            if (!ps.Ouvert) return;
+
+            tmrReception.Enabled = false;
+
+
+            while (ps.DonneesALire() >= tailleTrame + 2)
+            {
+                ps.LireOctet(ref octet);
+                if (octet == 0x5A)
+                {
+                    ps.LireOctet(ref octet);
+                    if (octet == 0xA5)
+                    {
+                        
+                        ps.LireOctets(tableau, 0, tailleTrame);
+
+                        cm = ((int)tableau[0] << 8) + tableau[1];
+
+                        lblDistance.Text = cm.ToString() + " cm";
+                    }
+                }
+            }
+
+            tmrReception.Enabled = true;
         }
     }
 }
